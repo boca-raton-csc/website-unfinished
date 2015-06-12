867c7f52e8af4952a466a475855f8b8dbc062abe
gabtimotei <gabtimotei@gmail.com>
2015-06-12 14:07:46 -0400

    Lolz
    Full website...

---
+++ Sites/CMS/index.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('includes/connection.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CMS Tutorial</title>
+        <link rel="stylesheet" href="assets/style.css" />
+    </head>
+    
+    <body>
+        <div class="container">
+            <a href="index.php" id="logo">CMS</a>
+            
+            <ol>
+                <li><a href="article.php?id=1">Article Title</a> - <small>posted Jan 1st, 2015</small></li>
+            </ol>
+        </div>
+    </body>
+    
+</html>
---
+++ Sites/Teacher Info/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#openmenu').click(function() {
+        $('.dropdown-menu').fadeToggle(200);
+    });
+});
